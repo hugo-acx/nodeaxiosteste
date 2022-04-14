e63afc14f5b9a90c2f9eed1c762dfe0b93f174ad
matheuserpacx <matheus.machado@erpacx.com>
2022-04-14 09:48:49 -0300

    login

--- controllers/users.js
+++ controllers/users.js
@@ -14,18 +14,18 @@ const emp_estabModel = require( '../model/emp_estab' );
 exports.Login = async ( req, res ) => {
     const dados = await usersModel.Auth( req.body.usuario, req.body.senha, req.body.empresa, req.body.estabelecimento );
     console.log(dados);
-    // if ( dados.data.token.length > 0 ) {
-    //     axios.defaults.headers.Authorization = 'Bearer ' + dados.data.token;
-
-    //     req.session.user = dados.data;
-    //     const context = {
-    //         title: "Login",
-    //         data: dados.data,
-    //     };
-    //     res.redirect( "/" );
-    // } else {
-    //     res.redirect( '/login' );
-    // }
+    if ( dados.data.chave.length > 0 ) {
+        //axios.defaults.headers.Authorization = 'Bearer ' + dados.data.token;
+        req.session.user = dados.data;
+        const context = {
+            title: "Login",
+            data: dados.data,
+        };
+        res.redirect( "/" );
+    } else {
+        alert('login invalido..');
+        res.redirect( '/login' );
+    }
 };
 
 exports.Logout = ( req, res ) => {
@@ -44,7 +44,7 @@ exports.Lista = async ( req, res ) => {
 exports.GetEmpEstab = async ( req, res) => {
     console.log(req.body)
     const dados = await emp_estabModel.GetEmpEstab(req.body.login, req.body.senha);
-    console.log(dados);
+    //console.log(dados);
     res.send(dados); 
     //console.log(dados);
 }
